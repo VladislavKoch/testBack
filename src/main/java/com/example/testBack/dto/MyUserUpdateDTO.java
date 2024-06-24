ea8901117fa28cf436f103188cfe49b63e1b22c5
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-24 17:58:51 +0300

    add user update dto

---
+++ src/main/java/com/example/testBack/dto/MyUserUpdateDTO.java
@@ -0,0 +1,23 @@
+package com.example.testBack.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyUserUpdateDTO {
+
+    @NotNull(message = "You must provide username")
+    @Pattern(regexp = "[a-zA-Z]+", message = "Username must consist of letters of the Latin alphabet")
+    private String username;
+
+    private String name;
+}
