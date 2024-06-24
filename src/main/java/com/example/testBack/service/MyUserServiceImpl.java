39a87241a4058217db94cd6c9a3093d3c53f52c8
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-24 15:06:24 +0300

    add user service impl

---
+++ src/main/java/com/example/testBack/service/MyUserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.testBack.service;
+
+import com.example.testBack.entity.MyUser;
+import com.example.testBack.repository.MyUserRepository;
+import com.example.testBack.service.serviceInterface.MyUserService;
+import exception.MyUserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserServiceImpl implements MyUserService {
+    private final MyUserRepository userRepository;
+    @Override
+    public MyUser saveUser(MyUser user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public MyUser updateUser(MyUser updatedUser) {
+        MyUser obtainedUser = userRepository.findById(updatedUser.getId()).orElseThrow(MyUserNotFoundException::new);
+        obtainedUser.setName(updatedUser.getName());
+        obtainedUser.setUsername(updatedUser.getUsername());
+        return userRepository.save(obtainedUser);
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public MyUser findUserById(int id) {
+        return userRepository.findById(id).orElseThrow(MyUserNotFoundException::new);
+    }
+}
