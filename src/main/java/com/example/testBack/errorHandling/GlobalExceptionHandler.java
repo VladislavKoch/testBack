e822a9be839932bb8a9b8066e525558b7b4e85a6
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-24 18:28:04 +0300

    add global exception handling functionality

--- src/main/java/com/example/testBack/errorHandling/GlobalExceptionHandler.java
+++ src/main/java/com/example/testBack/errorHandling/GlobalExceptionHandler.java
@@ -1,24 +1,32 @@
 package com.example.testBack.errorHandling;
 
+
 import com.example.testBack.dto.ErrorResponseDTO;
+import com.example.testBack.dto.InternalErrorDTO;
 import com.example.testBack.exception.DataIsNotCorrectException;
 import com.example.testBack.exception.MyUserAuthenticationException;
 import com.example.testBack.exception.MyUserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler({MyUserNotFoundException.class, DataIsNotCorrectException.class,
             MyUserAuthenticationException.class})
     protected ResponseEntity<?> handleRestControllersExceptions(ResponseStatusException ex) {
         log.debug(ex.getReason());
         return new ResponseEntity<>(new ErrorResponseDTO(ex.getReason()), ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<?> handleAllAnotherExceptions(Exception ex) {
+        log.debug(ex.getMessage());
+        return new ResponseEntity<>(new InternalErrorDTO(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
