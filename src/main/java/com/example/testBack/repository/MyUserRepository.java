4ec03f274a32a84f40a17d80b246b5407e13df30
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-24 15:43:49 +0300

    add exists by email & by username checking methods

--- src/main/java/com/example/testBack/repository/MyUserRepository.java
+++ src/main/java/com/example/testBack/repository/MyUserRepository.java
@@ -4,4 +4,6 @@ import com.example.testBack.entity.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MyUserRepository extends JpaRepository<MyUser, Integer> {
+    public boolean existsByEmail(String email);
+    public boolean existsByUsername(String username);
 }
