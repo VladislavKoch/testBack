a824f15c6a49cee710c130910c0384638164c625
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-25 10:35:00 +0300

    add film service impl

---
+++ src/main/java/com/example/testBack/service/FilmServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.testBack.service;
+
+import com.example.testBack.entity.Film;
+import com.example.testBack.repository.FilmRepository;
+import com.example.testBack.service.serviceInterface.FilmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class FilmServiceImpl implements FilmService {
+    private final FilmRepository filmRepository;
+
+    @Override
+    public Set<Film> saveNewFilms(Set<Film> obtainedFilms) {
+        Set<Film> savedFilms = new HashSet<>(filmRepository.findAll());
+        if (!savedFilms.isEmpty()) {
+            obtainedFilms.removeAll(savedFilms);
+        }
+        if (!obtainedFilms.isEmpty()) {
+            filmRepository.saveAll(obtainedFilms);
+        }
+        return obtainedFilms;
+    }
+}
