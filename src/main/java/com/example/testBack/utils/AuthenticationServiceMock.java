c9d2c18e608988ace3e32a5d29f28fa6ae073730
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-25 16:04:14 +0300

    remove unused

--- src/main/java/com/example/testBack/utils/AuthenticationServiceMock.java
+++ src/main/java/com/example/testBack/utils/AuthenticationServiceMock.java
@@ -1,12 +1,24 @@
 package com.example.testBack.utils;
 
 import com.example.testBack.exception.MyUserAuthenticationException;
+import com.example.testBack.exception.MyUserNotFoundException;
+import com.example.testBack.repository.MyUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Component
 public class AuthenticationServiceMock {
-    public static void checkAuthentication(Integer headerId, int id) {
+    private final MyUserRepository userRepository;
+
+    public void checkAuthentication(Integer headerId, int id) {
         if (headerId == null) {
             throw new MyUserAuthenticationException("User-Id header is empty!");
-        } else if (headerId != id) {
+        }
+        if (!userRepository.existsById(headerId)) {
+            throw new MyUserNotFoundException();
+        }
+        if (headerId != id) {
             throw new MyUserAuthenticationException("User-Id header is not equals path id!");
         }
     }
