62765a89dedc35c4dc14bc450c38024bf7ba4717
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-06-25 16:02:55 +0300

    add favorite relation service impl

---
+++ src/main/java/com/example/testBack/service/FavoriteRelationServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.testBack.service;
+
+import com.example.testBack.entity.FavoriteRelation;
+import com.example.testBack.repository.FavoriteRelationRepository;
+import com.example.testBack.service.serviceInterface.FavoriteRelationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FavoriteRelationServiceImpl implements FavoriteRelationService {
+    private final FavoriteRelationRepository relationRepository;
+
+    @Override
+    public FavoriteRelation addRelation(FavoriteRelation relation) {
+        return relationRepository.save(relation);
+    }
+
+    @Override
+    public boolean existRelationByUserIdAndFilmId(int userId, int filmId) {
+        return relationRepository.existsFavoriteRelationByFilmIdAndUserId(filmId, userId);
+    }
+
+
+}
